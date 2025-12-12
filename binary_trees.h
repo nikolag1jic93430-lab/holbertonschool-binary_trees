c755078bd83020b58ad467a7db3bebce76c4bcd4
nikolag1jic93430-lab <nikolag1jic93430@gmail.com>
2025-12-12 10:39:45 +0100

    .h
    Updated header file to define listint_s structure and added sorting function prototypes.

--- binary_trees.h
+++ binary_trees.h
@@ -1,24 +1,25 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef SORT_H
+#define SORT_H
 
 #include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-struct binary_tree_s
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element
+ * @next: Pointer to the next element
+ */
+typedef struct listint_s
 {
 	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-};
-typedef struct binary_tree_s binary_tree_t;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
 void bubble_sort(int *array, size_t size);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *tree);
-void _binary_tree_delete(binary_tree_t *tree);
 
 #endif
